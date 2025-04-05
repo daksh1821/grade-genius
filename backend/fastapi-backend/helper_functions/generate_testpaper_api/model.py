8a7a4f12d3eb663c1acf14a834de96e2d1fb0f06
RAGHAV JUNEJA <raghavjuneja2391@gmail.com>
2025-04-05 19:41:01 +0530

    feat: updated code of prompts passed to llm

--- backend/fastapi-backend/helper_functions/generate_testpaper_api/model.py
+++ backend/fastapi-backend/helper_functions/generate_testpaper_api/model.py
@@ -1,6 +1,7 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import ChatPromptTemplate
 from helper_functions.generate_testpaper_api.template import prompt
+from utils.logger import logger
 def load_model():
     llm=ChatGoogleGenerativeAI(
         model="gemini-2.0-flash-latest",
@@ -10,15 +11,22 @@ def load_model():
         max_retries=2
     )
     return llm
-def handle_user_conversations(number_of_questions,context):
-    # probably
+def handle_user_conversations(question_types, context, class_level, chapter_background):
+    logger.info("Loading model...")
     llm=load_model()
     chain=prompt|llm
-    output=chain.invoke(
+    logger.info("Generating questions with the following parameters:")
+    logger.info(f"Question Types: {question_types}")
+    logger.info(f"Class Level: {class_level}")
+    logger.info(f"Chapter Background: {chapter_background}")
+    output = chain.invoke(
         {
-            "number_of_questions":number_of_questions,
-            "context":
-            context
+            "question_types": question_types,
+            "context": context,
+            "class_level": class_level,
+            "chapter_background": chapter_background,
         }
     )
+
+    logger.info("Questions generated successfully.")
     return output
