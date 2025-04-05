7e13c88466da832adb99e2d0536db55872faa4f8
RAGHAV JUNEJA <raghavjuneja2391@gmail.com>
2025-04-05 12:27:35 +0530

    feat: add timetable upload API with OCR to convert images to JSON

---
+++ backend/fastapi-backend/routers/url.py
@@ -0,0 +1,52 @@
+from fastapi import(
+    APIRouter,
+    Request,
+    status,
+    HTTPException,
+    File,
+    UploadFile
+)
+from fastapi.responses import (
+    JSONResponse,
+    RedirectResponse
+)
+from logger import(
+    logger
+)
+from helper_functions.timetable_api.crop_to_table import auto_crop_table
+from helper_functions.timetable_api.ocr_functionality import process_cropped_image
+import shutil
+from pathlib import Path
+UPLOAD_DIR=Path("uploads")
+UPLOAD_DIR.mkdir(exist_ok=True)
+router=APIRouter()
+@router.get("/")
+async def redirect_to_docs():
+    return RedirectResponse(url="/docs")
+@router.post("/api/timetable/upload-image")
+async def upload_image(file: UploadFile = File(...)):
+    """
+    API endpoint to upload an image and automatically crop the timetable region.
+
+    Args:
+        file (UploadFile): Image file uploaded by the frontend.
+
+    Returns:
+        JSON response containing the path of the cropped image and its size.
+    """
+    try:
+        # Save the uploaded file to disk
+        image_path = UPLOAD_DIR / file.filename
+        with open(image_path, "wb") as buffer:
+            shutil.copyfileobj(file.file, buffer)
+
+        output_path = UPLOAD_DIR / f"cropped_{file.filename}"
+        cropped_path = auto_crop_table(str(image_path), str(output_path))
+
+        if cropped_path is None:
+            raise HTTPException(status_code=500,detail=f"Sorry , an error occured")
+        else:
+            return process_cropped_image(cropped_path)
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error processing image: {e}")
